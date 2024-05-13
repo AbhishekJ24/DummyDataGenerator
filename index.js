2ea63bcf07de449903381b58db9120bbd6de7a7e
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-05-14 04:07:24 +0530

    installed ejs, mongoose, express and completed functionalities

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
-import Dummy from "./models/Dummy"
+import { Dummy } from "./models/Dummy.js";
 
-await mongoose.connect("mongodb://localhost:27017/");
+
+await mongoose.connect("mongodb://localhost:27017/DummyData");
 
 const app = express()
 const port = 3000
 
-const names = ["Abhishek","Tom","Lucifer"]
-const salaries = [100000,50000,75000]
-const languages = ["Java","C","JavaScript"]
-const cities = ["New York","Los Angeles","British Columbia"]
-const boolManager = [true,false]
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+const names = ["Abhishek", "Tom", "Lucifer"]
+const salaries = [100000, 50000, 75000]
+const languages = ["Java", "C", "JavaScript"]
+const cities = ["New York", "Los Angeles", "British Columbia"]
+const boolManager = [true, false]
 
 app.get('/', (req, res) => {
-    const r1 = Math.floor(Math.random()*3)
-    const r2 = Math.floor(Math.random()*2)
-    const data = new Dummy({name:names[r1],salary:salaries[r1],language:languages[r1],city:cities[r1],isManager:boolManager[r2]});
+    res.render("Interface", {})
+})
+
+app.get('/generate', (req, res) => {
+    const r1 = Math.floor(Math.random() * 3)
+    const r2 = Math.floor(Math.random() * 2)
+    const data = new Dummy({ name: names[r1], salary: salaries[r1], language: languages[r1], city: cities[r1], isManager: boolManager[r2] });
     data.save()
-    res.render()
+    res.render("Interface", {})
+})
+
+app.get('/delete', async (req, res) => {
+    await Dummy.deleteMany({})
+    res.render("Interface", {})
 })
 
 app.listen(port, () => {
--- models/Dummy.js
+++ models/Dummy.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const dummySchema = new mongoose.schema({
+const dummySchema = new mongoose.Schema({
     name: String,
     salary: Number,
     language: String,
